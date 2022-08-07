1c87eb5b34be0939f87e06ef4862ad17a6999f13
Daniele Morganti <45139884+Dani-00git@users.noreply.github.com>
2022-08-07 17:39:10 +0200

    first commit
    creazione di interfaccia minimale statica per aggiunta task

--- todolist/mainwindow.cpp
+++ todolist/mainwindow.cpp
@@ -1,15 +1,27 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "QDateTime"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QDateTime time = QDateTime::currentDateTime();
+    ui->label_3->setText(time.toString("dd.MM.yyyy"));
+    ui->frame->setStyleSheet("QLabel{border-radius: 25px;}");
+    ui->textEdit->setPlaceholderText(QString("Name"));
+    ui->textEdit_2->setPlaceholderText(QString("Description"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
 }
 
