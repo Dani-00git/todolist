2db8a765267f4191bdaba844adf232e8593a5eb1
Daniele Morganti <45139884+Dani-00git@users.noreply.github.com>
2022-08-08 11:45:58 +0200

    Sviluppo delle prime interazioni grafiche

--- todolist/mainwindow.cpp
+++ todolist/mainwindow.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+#include "mainwindow.h""
 #include "./ui_mainwindow.h"
 #include "QDateTime"
 
@@ -8,10 +8,37 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     QDateTime time = QDateTime::currentDateTime();
+    setStyleSheet("background-color:white;");
+    ui->label_2->setStyleSheet("font-size: 17px;");
+    ui->label_3->setStyleSheet("font-size: 17px;");
     ui->label_3->setText(time.toString("dd.MM.yyyy"));
-    ui->frame->setStyleSheet("QLabel{border-radius: 25px;}");
     ui->textEdit->setPlaceholderText(QString("Name"));
     ui->textEdit_2->setPlaceholderText(QString("Description"));
+    ui->frame->setStyleSheet("background-color: white;"
+                             "border: 1px solid gray;"
+                             "border-radius:5px;");
+    ui->frame_4->setStyleSheet("background-color: white;"
+                               "border: 1px solid gray;"
+                               "border-radius:5px;");
+    ui->radioButton->setStyleSheet("border: 0px;");
+    ui->radioButton_2->setStyleSheet("border: 0px;");
+    ui->radioButton_3->setStyleSheet("border: 0px;");
+    ui->radioButton_4->setStyleSheet("border: 0px;");
+    ui->radioButton_5->setStyleSheet("border: 0px;");
+    ui->radioButton_6->setStyleSheet("border: 0px;");
+    ui->radioButton_7->setStyleSheet("border: 0px;");
+    ui->radioButton_8->setStyleSheet("border: 0px;");
+    ui->frame_5->setStyleSheet("background-color: white;"
+                               "border: 1px solid gray;"
+                               "border-radius:5px;");
+    ui->frame_4->hide();
+    ui->frame_5->hide();
+    ui->pushButton->setStyleSheet("background-color: white;"
+                                  "border: 1px solid gray;"
+                                  "border-radius:5px;");
+    ui->pushButton_5->setStyleSheet("background-color: white;"
+                                  "border: 1px solid gray;"
+                                  "border-radius:5px;");
 }
 
 MainWindow::~MainWindow()
@@ -22,6 +49,74 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+    ui->textEdit->setText("");
+    ui->textEdit_2->setText("");
+    ui->pushButton_2->setText("Today");
+    ui->pushButton_3->setText("Priority 1");
+    ui->frame_4->hide();
+    ui->frame_5->hide();
+}
 
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->frame_4->show();
+    ui->frame_5->hide();
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->frame_5->show();
+    ui->frame_4->hide();
+
+}
+
+
+void MainWindow::on_radioButton_clicked()
+{
+    ui->pushButton_2->setText("Today");
+}
+
+
+void MainWindow::on_radioButton_2_clicked()
+{
+    ui->pushButton_2->setText("Tomorrow");
+}
+
+
+void MainWindow::on_radioButton_3_clicked()
+{
+    ui->pushButton_2->setText("This Week");
+}
+
+
+void MainWindow::on_radioButton_4_clicked()
+{
+    ui->pushButton_2->setText("No Date");
+}
+
+
+void MainWindow::on_radioButton_5_clicked()
+{
+    ui->pushButton_3->setText("Priority 1");
+}
+
+
+void MainWindow::on_radioButton_6_clicked()
+{
+    ui->pushButton_3->setText("Priority 2");
+}
+
+
+void MainWindow::on_radioButton_7_clicked()
+{
+    ui->pushButton_3->setText("Priority 3");
+}
+
+
+void MainWindow::on_radioButton_8_clicked()
+{
+    ui->pushButton_3->setText("Priority 4");
 }
 
--- todolist/mainwindow.h
+++ todolist/mainwindow.h
@@ -18,6 +18,26 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_radioButton_2_clicked();
+
+    void on_radioButton_3_clicked();
+
+    void on_radioButton_4_clicked();
+
+    void on_radioButton_5_clicked();
+
+    void on_radioButton_6_clicked();
+
+    void on_radioButton_7_clicked();
+
+    void on_radioButton_8_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
